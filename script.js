080157a5a7b7080dfaaac481500ab7b1e8c4420c
ogasawarayui <ogasawara.yui@outlook.com>
2023-12-18 20:39:32 +0900

    js8

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Q1 xがyより大きい時にifの処理へ入るようにしてください。*/
+var x = 4
+var y = 2
+if (x > y) {
+  console.log("xはyより大きい");
+}
+/* Q2 xがyより小さい時にifの処理へ入るようにしてください。*/
+var x = 1
+var y = 3
+if (x < y) {
+  console.log("xはyより小さい");
+}
+/* Q3 xがy以上の時にifの処理へ入るようにしてください。*/
+var x = 8
+var y = 6
+if (x >= y) {
+  console.log("xはyより大きいまたは等しい");
+}
+/* Q4 xがy以下の時にifの処理へ入るようにしてください。*/
+var x = 5
+var y = 7
+if (x <= y) {
+  console.log("xはyより小さいまたは等しい");
+}
+
